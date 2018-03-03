5c3726913a8363987fa2989d8974e9cd25cc90aa
md9663386 <37010477+md9663386@users.noreply.github.com>
2018-03-03 12:11:47 +0530

    Update Github_sample.java
    test

--- Github_sample.java
+++ Github_sample.java
@@ -3,7 +3,9 @@ public class Github_sample {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println();
+		System.out.println("testing");
+		System.out.println("github");
+		
 
 	}
 
